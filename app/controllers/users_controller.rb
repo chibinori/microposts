18468ff222bef907438b96aac892d4bfd051ca83
Noriaki Sakai <noriaki.sakai.develop@gmail.com>
2015-11-23 11:58:18 +0000

    modify user-profile(delete checking user-id  in show method)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,13 +3,6 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
-    
-    if @user != current_user
-      # ログイン中のユーザではないユーザ情報を表示しようとしているので
-      # ホーム画面へリダイレクト
-      flash[:danger] = "Invalid operation detected!"
-      redirect_to root_url
-    end
   end
   
   def new
