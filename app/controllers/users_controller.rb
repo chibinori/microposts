0b8939a05380c11928841e0dc152efd7be2cdbe9
Noriaki Sakai <noriaki.sakai.develop@gmail.com>
2015-11-24 12:32:20 +0000

    finish followings-followers kadai9

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -53,6 +53,16 @@ class UsersController < ApplicationController
     end
   end
   
+  def followings
+    @user = User.find(params[:id])
+    @relational_users = @user.following_users
+  end
+  
+  def followers
+    @user = User.find(params[:id])
+    @relational_users = @user.follower_users
+  end
+  
   private
 
   def user_params
